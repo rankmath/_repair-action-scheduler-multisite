959cb6842762c4d44cdc2a2505505ab34b290d53
Balazs P <balazs.p@mythemeshop.com>
2021-11-20 16:20:31 +0100

    Version 1.1
    Update DB Schema

--- repair-action-scheduler-multisite.php
+++ repair-action-scheduler-multisite.php
@@ -9,9 +9,9 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Repair Action Scheduler - Multisite version
- * Version:           1.0
+ * Version:           1.1
  * Plugin URI:        https://s.rankmath.com/home
- * Description:       Fix database errors related to the Action Scheduler library on a multisite WP install. The plugin checks and creates the tables necessary for Action Scheduler version 3.0.16 when a new site is created. This plugin needs to stay activated on the site to avoid the error related to the Action Scheduler.
+ * Description:       Fix database errors related to the Action Scheduler library on a multisite WP install. The plugin checks and creates the tables necessary for Action Scheduler version 3.3.0 when a new site is created. This plugin needs to stay activated on the site to avoid the error related to the Action Scheduler.
  * Author:            Rank Math
  * Author URI:        https://s.rankmath.com/home
  * License:           GPL-2.0+
@@ -32,10 +32,18 @@ class Repair_Action_Scheduler_Mu {
 	const GROUPS_TABLE  = 'actionscheduler_groups';
 	const LOG_TABLE     = 'actionscheduler_logs';
 
+	/**
+	 * Class constructor.
+	 */
 	public function __construct() {
 		add_action( 'wp_insert_site', array( $this, 'create_tables' ), -10, 1 );
 	}
 
+	/**
+	 * Create tables.
+	 *
+	 * @param \WP_Site $site Site object.
+	 */
 	public function create_tables( $wp_site ) {
 		$this->tables = array(
 			self::ACTIONS_TABLE => 'action_id',
@@ -51,6 +59,14 @@ class Repair_Action_Scheduler_Mu {
 		}
 	}
 
+	/**
+	 * Check if table exists.
+	 *
+	 * @param string $table Table name.
+	 * @param int    $blog_id Site ID.
+	 *
+	 * @return bool
+	 */
 	private function table_exists( $table, $blog_id ) {
 		global $wpdb;
 		$prefix = $wpdb->get_blog_prefix( $blog_id );
@@ -58,11 +74,25 @@ class Repair_Action_Scheduler_Mu {
 		return (bool) $tables;
 	}
 
+	/**
+	 * Create table.
+	 *
+	 * @param string $table Table name.
+	 * @param int    $blog_id Site ID.
+	 */
 	private function create_table( $table, $blog_id ) {
 		global $wpdb;
 		$wpdb->query( $this->get_table_schema( $table, $blog_id ) ); // phpcs:ignore
 	}
 
+	/**
+	 * Get table schema.
+	 *
+	 * @param string $table Table name.
+	 * @param int    $blog_id Site ID.
+	 *
+	 * @return string
+	 */
 	private function get_table_schema( $table, $blog_id ) {
 		global $wpdb;
 		$prefix = $wpdb->get_blog_prefix( $blog_id );
@@ -70,6 +100,7 @@ class Repair_Action_Scheduler_Mu {
 		$table_name       = $prefix . $table;
 		$charset_collate  = $wpdb->get_charset_collate();
 		$max_index_length = 191; // @see wp_get_db_schema()
+		$default_date     = '0000-00-00 00:00:00';
 		switch ( $table ) {
 
 			case self::ACTIONS_TABLE:
@@ -77,14 +108,14 @@ class Repair_Action_Scheduler_Mu {
 					action_id bigint(20) unsigned NOT NULL auto_increment,
 					hook varchar(191) NOT NULL,
 					status varchar(20) NOT NULL,
-					scheduled_date_gmt datetime NOT NULL default '0000-00-00 00:00:00',
-					scheduled_date_local datetime NOT NULL default '0000-00-00 00:00:00',
+					scheduled_date_gmt datetime NULL default '${default_date}',
+					scheduled_date_local datetime NULL default '${default_date}',
 					args varchar($max_index_length),
 					schedule longtext,
 					group_id bigint(20) unsigned NOT NULL default '0',
 					attempts int(11) NOT NULL default '0',
-					last_attempt_gmt datetime NOT NULL default '0000-00-00 00:00:00',
-					last_attempt_local datetime NOT NULL default '0000-00-00 00:00:00',
+					last_attempt_gmt datetime NULL default '${default_date}',
+					last_attempt_local datetime NULL default '${default_date}',
 					claim_id bigint(20) unsigned NOT NULL default '0',
 					extended_args varchar(8000) DEFAULT NULL,
 					PRIMARY KEY  (action_id),
@@ -94,13 +125,13 @@ class Repair_Action_Scheduler_Mu {
 					KEY args (args($max_index_length)),
 					KEY group_id (group_id),
 					KEY last_attempt_gmt (last_attempt_gmt),
-					KEY claim_id (claim_id)
+					KEY `claim_id_status_scheduled_date_gmt` (`claim_id`, `status`, `scheduled_date_gmt`)
 					) $charset_collate";
 
 			case self::CLAIMS_TABLE:
 				return "CREATE TABLE {$table_name} (
 						claim_id bigint(20) unsigned NOT NULL auto_increment,
-						date_created_gmt datetime NOT NULL default '0000-00-00 00:00:00',
+						date_created_gmt datetime NULL default '${default_date}',
 						PRIMARY KEY  (claim_id),
 						KEY date_created_gmt (date_created_gmt)
 						) $charset_collate";
@@ -118,8 +149,8 @@ class Repair_Action_Scheduler_Mu {
 						log_id bigint(20) unsigned NOT NULL auto_increment,
 						action_id bigint(20) unsigned NOT NULL,
 						message text NOT NULL,
-						log_date_gmt datetime NOT NULL default '0000-00-00 00:00:00',
-						log_date_local datetime NOT NULL default '0000-00-00 00:00:00',
+						log_date_gmt datetime NULL default '${default_date}',
+						log_date_local datetime NULL default '${default_date}',
 						PRIMARY KEY  (log_id),
 						KEY action_id (action_id),
 						KEY log_date_gmt (log_date_gmt)
@@ -131,4 +162,7 @@ class Repair_Action_Scheduler_Mu {
 	}
 }
 
+/**
+ * Initialize the plugin.
+ */
 $repair_action_scheduler = new Repair_Action_Scheduler_Mu();
